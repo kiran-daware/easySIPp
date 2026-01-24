54823e2c56dd02e535268e49e86447d62481d66e
Kiran Daware <kirandaware@outlook.com>
2026-01-24 13:38:28 +0530

    Added new context processor for VersionInfo and updated footer template to display version information.

---
+++ easySIPp/__init__.py
@@ -0,0 +1,9 @@
+"""
+easySIPp Version Information
+"""
+
+__version__ = '2.0.01'
+__release_date__ = '2025-01-23'
+__app_name__ = 'easySIPp'
+__author__ = 'Kiran Daware'
+__url__ = 'https://dkiran.net'
