6b85cfb5fe0223e5dd664079ebc7b01cc5cc714e
Kiran Daware <kirandaware@outlook.com>
2026-01-26 08:52:39 +0530

    Release Version 2.0.26

--- easySIPp/__init__.py
+++ easySIPp/__init__.py
@@ -2,8 +2,8 @@
 easySIPp Version Information
 """
 
-__version__ = '2.0.01'
-__release_date__ = '2025-01-23'
+__version__ = '2.0.26'
+__release_date__ = '2026-01-26'
 __app_name__ = 'easySIPp'
 __author__ = 'Kiran Daware'
 __url__ = 'https://dkiran.net'
